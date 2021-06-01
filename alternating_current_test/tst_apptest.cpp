f89464d0c57c78e8b188a3eeebc69e9553eb924c
MikhailSklyarov1 <mikhail.29.06.2001@gmail.com>
2021-06-01 21:01:49 +0400

    Добавлен тест для функции cyclicCurrentsCalculation

--- alternating_current_test/tst_apptest.cpp
+++ alternating_current_test/tst_apptest.cpp
@@ -750,7 +750,292 @@ void AppTest::test_calculateResistance_data()
     QTest::newRow("Резисторы, катушки, конденсаторы") << test7 <<results[6];
 }
 
+void AppTest::doData()
+{
+    //ДАННЫЕ ДЛЯ ФУНКЦИЙ test_cyclicCurrentsCalculation
+    //test_calculaterRealCurrents
 
+
+    b_list1[0].insert(1, b.setResistance(ComplexVal(20.0, 4.0)).setVoltage(30.0).setNodes("", ""));
+    b_list2[0].insert(1, b.setCurrent(1.04));
+    contour.insert(1,1);
+    c_list[0].append(contour);
+
+    ccurr_list[0].append(ComplexVal(1.44, -0.29));
+
+    contour.clear();
+
+
+    b_list1[1].insert(1, b.setResistance(ComplexVal(10.0, 10.0)).setVoltage(0.0).setNodes("a", "b"));
+    b_list2[1].insert(1, b.setCurrent(1.49977));
+    b_list1[1].insert(2, b.setResistance(ComplexVal(15.0, -5.0)).setVoltage(0).setNodes("b", "c"));
+    b_list2[1].insert(2, b.setCurrent(0.35778));
+    b_list1[1].insert(3, b.setResistance(ComplexVal(5.0, 0.5)).setVoltage(0).setNodes("b", "c"));
+    b_list2[1].insert(3, b.setCurrent(1.13));
+    b_list1[1].insert(4, b.setResistance(ComplexVal(10.0, -5.0)).setVoltage(0).setNodes("c", "a"));
+    b_list2[1].insert(4, b.setCurrent(1.46));
+    b_list1[1].insert(5, b.setResistance(ComplexVal(0, 0)).setVoltage(30.0).setNodes("a", "b"));
+    b_list2[1].insert(5, b.setCurrent(2.51));
+
+    contour.insert(1, 1);
+    contour.insert(5, -1);
+    c_list[1].append(contour);
+    contour.clear();
+
+    contour.insert(4, -1);
+    contour.insert(2, -1);
+    contour.insert(5, -1);
+    c_list[1].append(contour);
+    contour.clear();
+
+    contour.insert(3, 1);
+    contour.insert(2, -1);
+    c_list[1].append(contour);
+    contour.clear();
+
+    ccurr_list[1].append(ComplexVal(-1.5, 1.5));
+    ccurr_list[1].append(ComplexVal(-1.95, -0.68));
+    ccurr_list[1].append(ComplexVal(1.55, 0.37));
+
+
+    b_list1[2].insert(1, b.setResistance(ComplexVal(20.0, 0)).setVoltage(0.0).setNodes("a", "b"));
+    b_list2[2].insert(1, b.setCurrent(1.66145));
+    b_list1[2].insert(2, b.setResistance(ComplexVal(25.0, 0)).setVoltage(0).setNodes("a", "b"));
+    b_list2[2].insert(2, b.setCurrent(0.50904));
+    b_list1[2].insert(3, b.setResistance(ComplexVal(20.0, 0)).setVoltage(0).setNodes("a", "c"));
+    b_list2[2].insert(3, b.setCurrent(1.0605));
+    b_list1[2].insert(4, b.setResistance(ComplexVal(15.0, 0)).setVoltage(0).setNodes("b", "c"));
+    b_list2[2].insert(4, b.setCurrent(0.5656));
+    b_list1[2].insert(5, b.setResistance(ComplexVal(5.0, 0)).setVoltage(0).setNodes("b", "c"));
+    b_list2[2].insert(5, b.setCurrent(0.58681));
+    b_list1[2].insert(6, b.setResistance(ComplexVal(0, 0)).setVoltage(30.0).setNodes("c", "a"));
+    b_list2[2].insert(6, b.setCurrent(2.21291));
+
+    contour.insert(1, 1);
+    contour.insert(2, -1);
+    c_list[2].append(contour);
+    contour.clear();
+
+    contour.insert(1, 1);
+    contour.insert(4, 1);
+    contour.insert(6, 1);
+    c_list[2].append(contour);
+    contour.clear();
+
+
+    contour.insert(1, 1);
+    contour.insert(5, 1);
+    contour.insert(6, 1);
+    c_list[2].append(contour);
+    contour.clear();
+
+    contour.insert(3, 1);
+    contour.insert(6, 1);
+    c_list[2].append(contour);
+    contour.clear();
+
+    ccurr_list[2].append(ComplexVal(0.72, 0));
+    ccurr_list[2].append(ComplexVal(0.8, 0));
+    ccurr_list[2].append(ComplexVal(0.83, 0));
+    ccurr_list[2].append(ComplexVal(1.5, 0));
+
+
+    b_list1[3].insert(1, b.setResistance(ComplexVal(30.0, 0)).setVoltage(0.0).setNodes("a", "c"));
+    b_list2[3].insert(1, b.setCurrent(0.48076));
+    b_list1[3].insert(2, b.setResistance(ComplexVal(20.0, 0)).setVoltage(0).setNodes("a", "c"));
+    b_list2[3].insert(2, b.setCurrent(0.71407));
+    b_list1[3].insert(3, b.setResistance(ComplexVal(10.0, 0)).setVoltage(0).setNodes("b", "a"));
+    b_list2[3].insert(3, b.setCurrent(1.19483));
+    b_list1[3].insert(4, b.setResistance(ComplexVal(10.0, 0)).setVoltage(30).setNodes("c", "b"));
+    b_list2[3].insert(4, b.setCurrent(2.50278));
+    b_list1[3].insert(5, b.setResistance(ComplexVal(20, 0)).setVoltage(0.0).setNodes("b", "c"));
+    b_list2[3].insert(5, b.setCurrent(1.30795));
+
+    contour.insert(1, -1);
+    contour.insert(2, 1);
+    c_list[3].append(contour);
+    contour.clear();
+
+    contour.insert(1, -1);
+    contour.insert(3, -1);
+    contour.insert(4, -1);
+    c_list[3].append(contour);
+    contour.clear();
+
+    contour.insert(1, -1);
+    contour.insert(3, -1);
+    contour.insert(5, 1);
+    c_list[3].append(contour);
+    contour.clear();
+
+    ccurr_list[3].append(ComplexVal(1.01, 0));
+    ccurr_list[3].append(ComplexVal(-3.54, 0));
+    ccurr_list[3].append(ComplexVal(1.85, 0));
+
+
+    b_list1[4].insert(1, b.setResistance(ComplexVal(10.0, 10.0)).setVoltage(0.0).setNodes("a", "b"));
+    b_list2[4].insert(1, b.setCurrent(1.49977));
+    b_list1[4].insert(2, b.setResistance(ComplexVal(15.0, -5.0)).setVoltage(0).setNodes("b", "c"));
+    b_list2[4].insert(2, b.setCurrent(0.36));
+    b_list1[4].insert(3, b.setResistance(ComplexVal(5.0, 0.5)).setVoltage(0).setNodes("b", "c"));
+    b_list2[4].insert(3, b.setCurrent(1.13));
+    b_list1[4].insert(4, b.setResistance(ComplexVal(10.0, -5.0)).setVoltage(0).setNodes("c", "a"));
+    b_list2[4].insert(4, b.setCurrent(1.46));
+    b_list1[4].insert(5, b.setResistance(ComplexVal(0, 0)).setVoltage(30.0).setNodes("a", "b"));
+    b_list2[4].insert(5, b.setCurrent(2.51));
+
+    contour.insert(1, 1);
+    contour.insert(5, -1);
+    c_list[4].append(contour);
+    contour.clear();
+
+    contour.insert(4, 1);
+    contour.insert(2, 1);
+    contour.insert(5, 1);
+    c_list[4].append(contour);
+    contour.clear();
+
+    contour.insert(3, 1);
+    contour.insert(2, -1);
+    c_list[4].append(contour);
+    contour.clear();
+
+    ccurr_list[4].append(ComplexVal(-1.5, 1.5));
+    ccurr_list[4].append(ComplexVal(1.95, 0.68));
+    ccurr_list[4].append(ComplexVal(1.55, 0.37));
+
+
+    b_list1[5].insert(1, b.setResistance(ComplexVal(10.0, 10.0)).setVoltage(0.0).setNodes("a", "b"));
+    b_list2[5].insert(1, b.setCurrent(1.49977));
+    b_list1[5].insert(2, b.setResistance(ComplexVal(15.0, -5.0)).setVoltage(0).setNodes("b", "c"));
+    b_list2[5].insert(2, b.setCurrent(0.36));
+    b_list1[5].insert(3, b.setResistance(ComplexVal(5.0, 0.5)).setVoltage(0).setNodes("b", "c"));
+    b_list2[5].insert(3, b.setCurrent(1.13));
+    b_list1[5].insert(4, b.setResistance(ComplexVal(10.0, -5.0)).setVoltage(0).setNodes("c", "a"));
+    b_list2[5].insert(4, b.setCurrent(1.46));
+    b_list1[5].insert(5, b.setResistance(ComplexVal(0, 0)).setVoltage(30.0).setNodes("a", "b"));
+    b_list2[5].insert(5, b.setCurrent(2.51));
+
+    contour.insert(1, -1);
+    contour.insert(5, 1);
+    c_list[5].append(contour);
+    contour.clear();
+
+    contour.insert(4, -1);
+    contour.insert(2, -1);
+    contour.insert(5, -1);
+    c_list[5].append(contour);
+    contour.clear();
+
+    contour.insert(3, -1);
+    contour.insert(2, 1);
+    c_list[5].append(contour);
+    contour.clear();
+
+    ccurr_list[5].append(ComplexVal(1.5, -1.5));
+    ccurr_list[5].append(ComplexVal(-1.95, -0.68));
+    ccurr_list[5].append(ComplexVal(-1.55, -0.37));
+
+
+    b_list1[6].insert(1, b.setResistance(ComplexVal(10.0, 10)).setVoltage(0.0).setNodes("a", "c"));
+    b_list2[6].insert(1, b.setCurrent(0.35));
+    b_list1[6].insert(2, b.setResistance(ComplexVal(15.0, -5)).setVoltage(0).setNodes("a", "c"));
+    b_list2[6].insert(2, b.setCurrent(0.9));
+    b_list1[6].insert(3, b.setResistance(ComplexVal(5.0, 0.5)).setVoltage(0).setNodes("b", "a"));
+    b_list2[6].insert(3, b.setCurrent(0.55));
+    b_list1[6].insert(4, b.setResistance(ComplexVal(10.0, -5)).setVoltage(30).setNodes("c", "b"));
+    b_list2[6].insert(4, b.setCurrent(1.29));
+    b_list1[6].insert(5, b.setResistance(ComplexVal(10, 0)).setVoltage(0.0).setNodes("b", "c"));
+    b_list2[6].insert(5, b.setCurrent(0.75));
+
+
+    contour.insert(1, -1);
+    contour.insert(2, 1);
+    c_list[6].append(contour);
+    contour.clear();
+
+    contour.insert(2, 1);
+    contour.insert(3, 1);
+    contour.insert(4, 1);
+    c_list[6].append(contour);
+    contour.clear();
+
+    contour.insert(4, 1);
+    contour.insert(5, 1);
+    c_list[6].append(contour);
+    contour.clear();
+
+    ccurr_list[6].append(ComplexVal(-0.46, 0.16));
+    ccurr_list[6].append(ComplexVal(-0.77, 0.14));
+    ccurr_list[6].append(ComplexVal(-0.99, 0.37));
+
+
+    b_list1[7].insert(1, b.setResistance(ComplexVal(10.0, 10.0)).setVoltage(0.0).setNodes("a", "c"));
+    b_list1[7].insert(2, b.setResistance(ComplexVal(10.0, 10.0)).setVoltage(0).setNodes("a", "c"));
+    b_list1[7].insert(3, b.setResistance(ComplexVal(10.0, 10.0)).setVoltage(0).setNodes("b", "a"));
+    b_list1[7].insert(4, b.setResistance(ComplexVal(10.0, 10.0)).setVoltage(30).setNodes("c", "b"));
+    b_list1[7].insert(5, b.setResistance(ComplexVal(10.0, 10.0)).setVoltage(0.0).setNodes("b", "c"));
+
+
+    contour.insert(1, -1);
+    contour.insert(2, 1);
+    c_list[7].append(contour);
+    contour.clear();
+
+    contour.insert(2, 1);
+    contour.insert(3, 1);
+    contour.insert(4, 1);
+    c_list[7].append(contour);
+    contour.clear();
+
+    contour.insert(4, 1);
+    contour.insert(5, 1);
+    c_list[7].append(contour);
+    contour.clear();
+
+    ccurr_list[7].append(ComplexVal(0.19, -0.19));
+    ccurr_list[7].append(ComplexVal(0.38, -0.38));
+    ccurr_list[7].append(ComplexVal(0.56, -0.56));
+
+}
+
+
+void AppTest::test_cyclicCurrentsCalculation()
+{
+    QVector<ComplexVal> c;
+    QFETCH(contourList, contours);
+    QFETCH(branchList, branches);
+    QFETCH(QVector<ComplexVal>, currents);
+
+    cyclicCurrentsCalculation(contours, branches, c);
+    bool b = true;
+    for(int i = 0; i < c.count() && c.count() == currents.count(); i++)
+    {
+
+        //допустимая погрешность в ручных расчетах - 0.5
+        if(!((c[i].real()-0.5 < currents[i].real() || c[i].imag()-0.5 < currents[i].imag())
+            && (currents[i].real() < c[i].real()+0.5 || currents[i].imag() < c[i].imag()+0.5)))
+            b = false;
+    }
+    QVERIFY2(compareVects(c, currents) || b, "Result checking failed");
+}
+
+void AppTest::test_cyclicCurrentsCalculation_data()
+{
+    QTest::addColumn<contourList>("contours");
+    QTest::addColumn<branchList>("branches");
+    QTest::addColumn<QVector<ComplexVal>>("currents");
+
+    QTest::newRow("В цепи один контур") << c_list[0] << b_list1[0] << ccurr_list[0];
+    QTest::newRow("Через каждую ветвь протекает не более двух контурных токов") << c_list[1] << b_list1[1] << ccurr_list[1];
+    QTest::newRow("Цепь содержит ветви, через которые протекает более двух контурных токов")<< c_list[2] << b_list1[2] << ccurr_list[2];
+    QTest::newRow("Цепь содержит ветви, через которые протекают все контурные токи") << c_list[3] << b_list1[3] << ccurr_list[3];
+    QTest::newRow("Все контурные токи движутся по часовой стрелке") << c_list[4] << b_list1[4] << ccurr_list[4];
+    QTest::newRow("Все контура движутся против часовой стрелки") << c_list[5] << b_list1[5] << ccurr_list[5];
+    QTest::newRow("Все смежные контура однонаправленны") << c_list[6] << b_list1[6] << ccurr_list[6];
+    QTest::newRow("Величина сопротивления на всех ветвях одинаковая") << c_list[7] << b_list1[7] << ccurr_list[7];
+
+}
 
 
 
