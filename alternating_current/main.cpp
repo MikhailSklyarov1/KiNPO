41d425a807089c14550623c7b09cf958f86091d3
MikhailSklyarov1 <mikhail.29.06.2001@gmail.com>
2021-06-12 17:17:08 +0400

    Добавлена реализация функции makingBranch

--- alternating_current/main.cpp
+++ alternating_current/main.cpp
@@ -141,3 +141,132 @@ void parseFileXML(char * filename, QMap<int, Branch> & branches, Graph & graph)
         }
     }
 }
+
+
+
+int makingBranch(TiXmlElement * xmlnode, Branch & branch, int * number,int * id_num)
+{
+    int count = 0;
+    QStringList wrongElements;
+    QString mes;
+    if ( xmlnode )
+    {
+        const char * begin = xmlnode->Attribute("begin");
+        const char * end = xmlnode->Attribute("end");
+
+        //Если начало ветви не указано
+        if(begin == NULL)
+        {
+            //Считать узел по умолчанию a
+            begin = "a";
+        }
+
+        //Если конец ветви не указан
+        if(end == NULL)
+        {
+            //Считать узел по умолчанию a
+            end = "a";
+        }
+
+        QString allow("qwertyuiopasdfghjklzxcvbnm0123456789"); //Допустимые элементы в названии узла
+
+        bool beginNodeCorrect =allow.contains(begin) || allow.isEmpty(); //Проверить узел, из которого ветвь исходит
+        bool endNodeCorrect =allow.contains(end) || allow.isEmpty(); //Проверить узел, в который ветвь заходит
+
+        //Если узлы некорректно названы
+        if(!(beginNodeCorrect && endNodeCorrect))
+        {
+            throw QString("В названии узлов должны присутствовать только латинские буквы нижнего регистра и цифры.");
+        }
+
+        //Считать с файла id
+        char  *_id = (char *)xmlnode->Attribute("id");
+
+        //Если id элемена не указано
+        if(_id == NULL)
+        {
+            //Считать индекс первого лемента эдиницей
+            _id = "1";
+        }
+
+        //Проиндексировать последующие элементы
+        char buff[20];
+        _id = itoa(*id_num, buff, 10);
+
+
+        double volt = 0;
+        Element e;
+        QVector<Element> elements;
+
+        //Считать резисторы
+        TiXmlElement* resistor = xmlnode->FirstChildElement( "resistor" );
+        while ( resistor )
+        {
+            try {
+                makingElement(resistor, e, RESISTOR, "резистор", "om", "Ом");
+            } catch (QString mes) {
+                wrongElements << mes;
+            }
+            elements.append(e);
+            resistor = resistor->NextSiblingElement( "resistor" );
+            count++;
+        }
+
+        //Считать катушки индуктивности
+        TiXmlElement* inductor = xmlnode->FirstChildElement( "inductor" );
+        while ( inductor )
+        {
+            try {
+                string str0 = "катушка индуктивности";
+                makingElement(inductor, e, INDUCTOR, "катушка индуктивности", "henry", "Генри");
+            } catch (QString mes) {
+                wrongElements << mes;
+            }
+            elements.append(e);
+            inductor = inductor->NextSiblingElement( "inductor" );
+            count++;
+        }
+
+        //Считать конденсаторы
+        TiXmlElement* capacitor = xmlnode->FirstChildElement( "capacitor" );
+        while ( capacitor )
+        {
+            try {
+                makingElement(capacitor, e, CAPACITOR, "конденсатор", "farad", "Фарад");
+            } catch (QString mes) {
+                wrongElements << mes;
+            }
+            elements.append(e);
+            capacitor = capacitor->NextSiblingElement( "capacitor" );
+            count++;
+        }
+
+        //Считать источники питания
+        TiXmlElement* source = xmlnode->FirstChildElement( "source" );
+        while ( source )
+        {
+            try {
+                makingElement(source, e, SOURCE, "источник ЭДС", "volt", "Вольт");
+            } catch (QString mes) {
+                wrongElements << mes;
+            }
+            volt = e.value;
+            elements.append(e);
+            source = source->NextSiblingElement( "source" );
+            count++;
+        }
+        branch = Branch(elements, begin, end, volt);	//узлы
+        *number = atoi(_id);
+    }
+
+    if(!wrongElements.isEmpty())
+    {
+        QString exception("Числовые значения для элементов: ");
+        QStringListIterator i(wrongElements);
+        while(i.hasNext())
+            exception += i.next() + ", ";
+        exception += QString(" некорректны. Должны быть в диапазоне [10^-6, 10^6].");
+        throw exception;
+    }
+    return count;
+}
